444ae35a6fd095f8ece1e81875d89965f0e7a80e
aaberk <aaberkane@iut.univ-paris8.fr>
2025-11-12 14:01:18 +0100

    Delete java directory

--- java/universite_paris8/iut/wad/sae_dev/Modele/Utilisables/UtilisationObjet.java
+++
@@ -1,8 +0,0 @@
-package universite_paris8.iut.wad.sae_dev.Modele.Utilisables;
-
-import universite_paris8.iut.wad.sae_dev.Modele.Jeu;
-import universite_paris8.iut.wad.sae_dev.Modele.Terraformer;
-
-public interface UtilisationObjet {
-    void utiliser(int x, int y, Jeu jeu, Terraformer terraformer);
-    }
